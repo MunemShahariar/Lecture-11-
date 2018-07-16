e52f243675f88a489b6035d7a3ce03b0f37cc7dc
Munem Shahariar <munemshahariar123@gmail.com>
2018-07-16 17:07:08 +0000

    latest update

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ var express = require('express');
 var app  = express();
 var server =  http.Server(app);
 
-app.get('/', function(req, res){
+app.get('/index', function(req, res){
   res.sendFile(__dirname+"/index.html");
 })  
 app.get('/about',function(req , res){
   res.sendFile(__dirname+"/about.html");
+});
+app.get('/form',function(req,res){
+  res.sendFile(__dirname+"/form.html");
+});
+app.post('/form',function(req , res){
+  var Username = req.body.Username;
+  var email = req.body.email;
+  console.log("input accepted : %s, %s" , Username , email);
 });
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
